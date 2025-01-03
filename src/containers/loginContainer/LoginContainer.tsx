07b486394ce0e50efabb3760d2ab213d39c65520
Yudhisthir Chavan <cavemandeveloper@gmail.com>
2025-01-03 05:52:52 +0530

    changed role to to take from env

--- src/containers/loginContainer/LoginContainer.tsx
+++ src/containers/loginContainer/LoginContainer.tsx
@@ -75,7 +75,7 @@ const LoginContainer = () => {
     const handleSubmit = async (payload:object) => {
         const updatedPayload = {
             ...payload,
-            role: Roles.ROLE_ADMIN,
+            role: import.meta.env.ROLE,
         };
         const loginPromise = axios.post(registrationLoginToggle ?
             api.USERS.REGISTER_NEW_USER : api.USERS.AUTHENTICATE_AND_GET_TOKEN, updatedPayload);
